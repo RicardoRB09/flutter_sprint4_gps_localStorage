1d97373f40bae6228f5e160134e4de97c95c2a9c
Ricardo RB <ricardo.racinesb@gmail.com>
2022-10-10 23:26:21 -0500

    feat: Androidmanifest and gps.dart TODOs done.

--- lib/device/gps.dart
+++ lib/device/gps.dart
@@ -1,24 +1,30 @@
 import 'package:geolocator/geolocator.dart';
 
-
 class GpsSensor {
-  Future<LocationPermission> get permissionStatus async => // Usando GeoLocator verifica el estado de los permisos
+  // Usando GeoLocator verifica el estado de los permisos
+  Future<LocationPermission> get permissionStatus async =>
+      await Geolocator.checkPermission();
 
-  Future<Position> get currentLocation async => // Usando GeoLocator obten la posicion actual
+  // Usando GeoLocator obten la posicion actual
+  Future<Position> get currentLocation async =>
+      await Geolocator.getCurrentPosition();
 
-  Future<LocationAccuracyStatus> get locationAccuracy async => // Usando GeoLocator verifica la precision de la ubicacion con soporte para web
+  // Usando GeoLocator verifica la precision de la ubicacion con soporte para web
+  Future<LocationAccuracyStatus> get locationAccuracy async =>
+      await Geolocator.getLocationAccuracy();
 
   Future<LocationPermission> requestPermission() async {
-    // TODO: Debes configurar correctamente el AndroidManifest.xml para Android:
+    // TODO Debes configurar correctamente el AndroidManifest.xml para Android:
     // 1. <uses-permission android:name="android.permission.ACCESS_FINE_LOCATION" />
     // 2. <uses-permission android:name="android.permission.ACCESS_COARSE_LOCATION" />
 
-    // TODO: Si usas iOS debes configurar el Info.plist usando Xcode
+    // TODO Si usas iOS debes configurar el Info.plist usando Xcode
     // 1. <key>NSLocationWhenInUseUsageDescription</key>
     //    <string>Esta aplicación necesita acceso a la ubicación cuando está abierta.</string>
     // 2. <key>NSLocationAlwaysUsageDescription</key>
     //    <string>Esta aplicación necesita acceso a la ubicación cuando está en segundo plano.</string>
 
-    // TODO: Usando GeoLocator solicita los permisos
+    // TODO Usando GeoLocator solicita los permisos
+    return await Geolocator.requestPermission();
   }
 }
